78536965e0e826b23c18c880a1c08a3cba0c08a2
beloquintana <belo.quintana@gmail.com>
2020-03-31 15:10:26 -0300

    Add Employee Test

--- SeleniumFramework/PageObject/EmployeePage.cs
+++ SeleniumFramework/PageObject/EmployeePage.cs
@@ -11,6 +11,11 @@ namespace SeleniumFramework.PageObject
     public class EmployeePage : BasePage
     {
         protected By Form = By.Id("formEmployee");
+        protected By NameInput = By.XPath("//*[@id='formEmployee']/div[2]/div[1]/input");
+        protected By EmailInput = By.XPath("//*[@id='formEmployee']/div[2]/div[2]/input");
+        protected By AddressTextArea = By.Id("address");
+        protected By PhoneInput = By.Id("phone");
+        protected By AddButton = By.Id("addButton");
 
         public EmployeePage(IWebDriver driver)
             : base(driver)
@@ -22,6 +27,15 @@ namespace SeleniumFramework.PageObject
         public bool FormIsPresent()
         {
             return WaitHelper.ElementIsPresent(Driver, Form, 2);
+        }
+
+        public void AddEmployee(string name, string email, string address, string phone)
+        {
+            Driver.FindElement(NameInput).SendKeys(name);
+            Driver.FindElement(EmailInput).SendKeys(email);
+            Driver.FindElement(AddressTextArea).SendKeys(address);
+            Driver.FindElement(PhoneInput).SendKeys(phone);
+            Driver.FindElement(AddButton).Click();
         }
 
         public bool IsSuccessAlertPresent()
---
+++ SeleniumFramework/Test/EmployeeTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using SeleniumFramework.PageObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumFramework.Test
+{
+    [TestFixture]
+    public class EmployeeTest : BaseTest
+    {
+        private EmployeePage employeePage;
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            LoginPage loginPage = new LoginPage(Driver);
+            employeePage = loginPage.LoginAs("admin", "admin123");
+        }
+
+        [Test]
+        public void AddEmployee()
+        {
+            employeePage.AddEmployee("Juan", "juan@gmail.com", "MTV", "598752022");
+            Assert.IsTrue(employeePage.IsSuccessAlertPresent());
+        }
+    }
+}
